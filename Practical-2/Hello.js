6b2bb3ec1cae06930ed8eda00e4d8877e8a1f08c
Shraaavaaaniii <140896891+Shraaavaaaniii@users.noreply.github.com>
2025-11-07 19:11:43 +0530

    Delete Practical-2 directory

--- Practical-2/Hello.js
+++
@@ -1,9 +0,0 @@
-let http = require('http');
-const hostname = '127.0.0.1';
-let server = http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type':'text/html'});
-    res.end("Hello World!")
-})
-server.listen(3000,hostname, ()=>{
-    console.log("Server is running at http://127.0.0.1:3000/");
-});
